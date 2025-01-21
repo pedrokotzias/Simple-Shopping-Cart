26270ea5830172aed947ca2c665ca4c94c57ac7a
Pedro Kotzias <pepeuk@gmail.com>
2025-01-20 21:01:04 -0300

    added a remove button next to the cart items so you can remove one by one

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 let productPricesArray = [];
+let cartItems = [];
+let nextId = 0;
 
 function getItems() {
     const items = document.getElementsByTagName('option');
@@ -11,7 +13,7 @@ function getQuantity() {
 }
 
 function getCart() {
-    return document.getElementsByClassName('carrinho__produtos');
+    return document.getElementsByClassName('carrinho__produtos')[0];
 }
 
 function findCartItem(cart, name) {
@@ -19,26 +21,45 @@ function findCartItem(cart, name) {
         .find(cartItem => cartItem.textContent.includes(name));
 }
 
-function updateCartItem(cartItem, price, quantity) {
+function updateCartItem(cartItem, price, quantity, index) {
     let existingQuantity = Math.round(parseFloat(cartItem.querySelector('.texto-azul:first-child').innerText));
     let newQuantity = existingQuantity + quantity;
     let newPrice = price * newQuantity;
 
     cartItem.querySelector('.texto-azul:first-child').innerText = `${newQuantity}x`;
-    cartItem.querySelector('.texto-azul:last-child').innerText = `R$${newPrice}`;
+    cartItem.querySelector(':nth-child(2)').innerText = `R$${newPrice}`;
 
+    cartItems[index].quantity = newQuantity;
+    cartItems[index].totalPrice = newPrice;
+    productPricesArray[index] = newPrice;
     return newPrice;
+}
+
+function createRemoveButton(id) {
+    const button = document.createElement('button');
+    button.innerText = 'X';
+    button.classList.add('remove-button');
+    button.addEventListener('click', () => removeCartItem(id));
+    return button;
 }
 
 function createNewCartItem(cart, name, price, quantity) {
     let totalValue = price * quantity;
+    let id = nextId++;
     productPricesArray.push(totalValue);
+
+    cartItems.push({ id, name, quantity, price, totalPrice: totalValue });
 
     const cartItem = document.createElement('section');
     cartItem.classList.add('carrinho__produtos__produto');
+    cartItem.setAttribute('data-id', id);
     cartItem.innerHTML = `<span class="texto-azul">${quantity}x</span> ${name} <span class="texto-azul">R$${totalValue}</span>`;
+    
+    const removeButton = createRemoveButton(id);
+    cartItem.appendChild(removeButton);
 
     cart.appendChild(cartItem);
+    updateTotal();
 }
 
 function createItemCart() {
@@ -46,18 +67,32 @@ function createItemCart() {
     let name = item[0];
     let price = Math.round(parseFloat(item[1].replace('R$', '')));
     let quantity = Math.round(parseFloat(getQuantity()));
-    const cart = getCart()[0];
+    const cart = getCart();
 
     let existingItem = findCartItem(cart, name);
 
     if (existingItem) {
-        let newPrice = updateCartItem(existingItem, price, quantity);
-
-        let index = Array.from(cart.getElementsByClassName('carrinho__produtos__produto'))
-            .indexOf(existingItem);
-        productPricesArray[index] = newPrice;
+        let index = cartItems.findIndex(cartItem => cartItem.name === name);
+        if (index !== -1) {
+            updateCartItem(existingItem, price, quantity, index);
+        }
     } else {
         createNewCartItem(cart, name, price, quantity);
+    }
+    updateTotal();
+}
+
+
+function removeCartItem(id) {
+    const cart = getCart();
+    const cartItem = cart.querySelector(`.carrinho__produtos__produto[data-id='${id}']`);
+    
+    const index = cartItems.findIndex(item => item.id === id);
+    if (index > -1) {
+        productPricesArray.splice(index, 1);
+        cartItems.splice(index, 1);
+        cart.removeChild(cartItem);
+        updateTotal();
     }
 }
 
@@ -77,8 +112,9 @@ function adicionar() {
 }
 
 function limpar() {
-    const cart = getCart()[0];
+    const cart = getCart();
     cart.innerHTML = '';
     productPricesArray = [];
+    cartItems = [];
     updateTotal();
 }
